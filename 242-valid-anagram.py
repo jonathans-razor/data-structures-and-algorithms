56f2e49e875ae7438a4dcd862a1a8e2b834308d2
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-10-10 13:24:52 -0400

    Oct-10-2023_1:24_PM

---
+++ 242-valid-anagram.py
@@ -0,0 +1,15 @@
+import sys
+
+def isAnagram(s1: str, s2: str) -> bool: 
+  return sorted(s1) == sorted(s2)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Please provide two strings as command line arguments.")
+    else:
+        s1 = sys.argv[1]
+        s2 = sys.argv[2]
+        if isAnagram(s1, s2):
+            print(f"{s1} and {s2} are anagrams of each other.")
+        else:
+            print(f"{s1} and {s2} are NOT anagrams of each other.")
