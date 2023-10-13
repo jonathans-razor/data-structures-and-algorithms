5117ef5f5fd675f514ecefb8251d7b66f5bcdd79
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-10-13 18:00:36 -0400

    Oct-13-2023_6:00_PM

--- 217-contains-duplicate.py
+++ 217-contains-duplicate.py
@@ -1,14 +1,14 @@
 import argparse
 
-def has_duplicates(arr):
-    return len(arr) != len(set(arr))
+def hasDuplicates(nums):
+    return len(nums) != len(set(nums))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Check if an array has duplicates.')
     parser.add_argument('arr', metavar='N', type=int, nargs='+',
                         help='an integer array')
     args = parser.parse_args()
-    if has_duplicates(args.arr):
+    if hasDuplicates(args.arr):
         print('The array contains duplicates.')
     else:
         print('The array does NOT contain duplicates.')
