c87267228e15eb685cbc780fb74049e7ed04cf2f
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-10-17 15:31:02 -0400

    Oct-17-2023_3:31_PM

--- read-file.js
+++
@@ -1,9 +0,0 @@
-const { readFile } = require('fs').promises;
-
-async function hello() {
-  const file = await readFile('hello.txt', 'utf8');
-  console.log(file.toString());
-}
-
-hello();
-console.log('start');
