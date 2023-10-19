3db3f38610fc7d9f8362ceccc5b9482e17b6591d
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-10-19 13:13:37 -0400

    Oct-19-2023_1:13_PM

--- echo-parameters.py
+++ echo-parameters.py
@@ -1,8 +1,5 @@
 import sys
 
-def isAnagram(s1: str, s2: str) -> bool: 
-  return sorted(s1) == sorted(s2)
-
 if __name__ == "__main__":
   if len(sys.argv) < 2:
     print("- Parameter(s) expected.")
