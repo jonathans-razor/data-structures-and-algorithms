f0da0c31a3627a9097140adc36e85379e90c100b
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-10-19 13:12:13 -0400

    Oct-19-2023_1:12_PM

--- echo-parameters.py
+++ echo-parameters.py
@@ -1,17 +1,10 @@
 import sys
 
-import sys
-
 def isAnagram(s1: str, s2: str) -> bool: 
   return sorted(s1) == sorted(s2)
 
 if __name__ == "__main__":
-  if len(sys.argv) != 3:
-    print("- Please provide two strings as command line arguments.")
+  if len(sys.argv) < 2:
+    print("- Parameter(s) expected.")
     exit(1)
-  s1 = sys.argv[1]
-  s2 = sys.argv[2]
-  if isAnagram(s1, s2):
-    print(f"{s1} and {s2} are anagrams of each other.")
-  else:
-    print(f"{s1} and {s2} are NOT anagrams of each other.")
+  print(sys.argv[1:])
