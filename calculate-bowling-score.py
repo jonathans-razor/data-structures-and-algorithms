31b8bc76b5503cfdd272681255d20d21cf9dbe62
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-10-25 00:27:16 -0400

    Oct-25-2023_12_27_16_AM

--- calculate-bowling-score.py
+++ calculate-bowling-score.py
@@ -2,25 +2,24 @@ import sys
 
 def calculate_score(rolls):
     total_score = 0
-    frame = 0
-    new_frame = True
-    for index, roll in enumerate(rolls):
-        if frame == 10:
-            break
-        if roll == 10:
-            total_score += rolls[index + 1]
-            total_score += rolls[index + 2]
-            frame += 1
-        elif not new_frame:
-            if rolls[index - 1] + roll == 10:
-                total_score += rolls[index + 1]
-                frame += 1
-            new_frame = True
+    frame = 1
+    roll_index = 0
+
+    while frame <= 10:
+        if rolls[roll_index] == 10:
+            total_score += 10 + rolls[roll_index + 1] + rolls[roll_index + 2]
+            roll_index += 1
+        elif rolls[roll_index] + rolls[roll_index + 1] == 10:
+            total_score += 10 + rolls[roll_index + 2]
+            roll_index += 2
         else:
-            new_frame = False
-        total_score += roll
+            total_score += rolls[roll_index] + rolls[roll_index + 1]
+            roll_index += 2
+
+        frame += 1
+
     return total_score
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     rolls = [int(x) for x in sys.argv[1:]]
     print(calculate_score(rolls))
