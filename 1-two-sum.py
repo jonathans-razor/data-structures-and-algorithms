3196b10a8c96c5fc6622f49f15c781190c7f55d1
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-10-24 22:23:39 -0400

    Oct-24-2023_10_23_39_PM

--- 1-two-sum.py
+++ 1-two-sum.py
@@ -1,14 +1,20 @@
-import sys
+import argparse
 
 def twoSum(nums, target):
-    destination_array = {}
-    for i, num in enumerate(nums):
-      #print(i, num)
-      if target - num in destination_array:
-        return [destination_array[target - num], i]
-      destination_array[num] = i
+    for i in range(len(nums)):
+        print("i: " +str(i))
+        for j in range(i+1, len(nums)):
+            print("j: " + str(j))
+            if nums[i] + nums[j] == target:
+                arr = []
+                arr.append(i)
+                arr.append(j)
+                return arr
 
-if __name__ == "__main__":
-    nums = list(map(int, sys.argv[1:-1]))
-    target = int(sys.argv[-1])
-    print(twoSum(nums, target))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Check if an array has duplicates.')
+    parser.add_argument('arr', metavar='N', type=int, nargs='+',
+                        help='an integer array')
+    args = parser.parse_args()
+    target = args.arr[-1]
+    print(twoSum(args.arr, target))
