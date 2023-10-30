3956657c7b4c6f4f5b28d7acd808c2c33c680a09
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-10-30 14:07:43 -0400

    Oct-30-2023_2_07_43_PM

--- string-manipulation.py
+++ string-manipulation.py
@@ -5,9 +5,14 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 def stringManipulation(s: str) -> str:
-    print('   input string: ' + s)
-    print('first character: ' + s[0])
-    print(' last character: ' + s[-1])
+    print('       input string: ' + s)
+    print('    first character: ' + s[0])
+    lengthOfString = len(s)
+    halfString = lengthOfString // 2
+    print('         halfString: ' + str(halfString))
+    middleString = s[halfString - 1:halfString + 2]
+    print('middle 3 characters: ' + middleString)
+    print('     last character: ' + s[-1])
     return s
 
 stringManipulation(sys.argv[1])
