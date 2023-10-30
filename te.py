68991cef16efa012e41fd833fb37bba4ba05f8f2
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-10-30 12:52:26 -0400

    Oct-30-2023_12_52_26_PM

--- te.py
+++ te.py
@@ -1,10 +1,10 @@
 import sys
 
-def reverseString(s: str) -> int:
-    return s[::-1]
-
 if len(sys.argv) < 2:
-    print("- Parameter(s) expected.")
-    sys.exit(1)
+  print("- Parameter(s) expected.")
+  sys.exit(1)
+
+def reverseString(s: str) -> int:
+  return s[::-1]
 
 print(reverseString(sys.argv[1]))
