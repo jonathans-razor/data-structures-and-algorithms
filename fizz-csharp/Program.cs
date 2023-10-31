87b7d605ecb251d227d4d0dec277c93e6376a4d4
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-10-31 16:57:03 -0400

    Oct-31-2023_4_57_03_PM

---
+++ fizz-csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            string output = "";
+            if (i % 3 == 0)
+                output += "Fizz";
+            if (i % 5 == 0)
+                output += "Buzz";
+            if (i % 7 == 0)
+                output += "Bazz";
+
+            Console.WriteLine(output != "" ? output : i.ToString());
+        }
+    }
+}
---
+++ reverse-string-csharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StringReversalApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide a string as an argument.");
+                return;
+            }
+
+            string originalString = args[0];
+            string reversedString = ReverseString(originalString);
+
+            Console.WriteLine($"Original String: {originalString}");
+            Console.WriteLine($"Reversed String: {reversedString}");
+        }
+
+        static string ReverseString(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
