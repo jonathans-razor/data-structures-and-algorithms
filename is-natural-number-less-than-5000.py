c3841d2ff42115209a2cba40f4d26d01ff1fb6c8
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-11-07 17:08:10 -0500

    Nov-7-2023_5_08_10_PM

---
+++ is-natural-number-less-than-5000.py
@@ -0,0 +1,14 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("- Parameter(s) expected.")
+    sys.exit(1)
+
+
+def isNaturalNumberLessThan5000(x: int) -> int:
+  if x.isdigit():
+    if int(x) <= 5000:
+      return 1
+  return 0
+
+print(isNaturalNumberLessThan5000(sys.argv[1]))
