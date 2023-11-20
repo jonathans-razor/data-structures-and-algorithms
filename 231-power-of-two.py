bce9b272b5fadbc46a86cbb96f7098ca202a0555
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-11-20 13:35:23 -0500

    Nov-20-2023_1_35_23_PM

--- 231-power-of-two.py
+++ 231-power-of-two.py
@@ -1,16 +1,17 @@
 import sys
 
 if len(sys.argv) < 2:
-  print("- Parameter(s) expected.")
-  sys.exit(1)
+    print("- Parameter(s) expected.")
+    sys.exit(1)
+
 
 def isPowerOfTwo(n: int) -> bool:
-  i = 0
-  while 2 ** i <= n:
-    if 2 ** i == n:
-      return True
-    i += 1
-  else: 
+    i = 0
+    while 2**i <= n:
+        if 2**i == n:
+            return True
+        i += 1
     return False
 
+#print("Given an integer n, return true if it is a power of two. Otherwise, return false.")
 print(isPowerOfTwo(int(sys.argv[1])))
