9a48dd6cc035c081117d23a6b85ad7f26ae2be71
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-11-21 16:00:06 -0500

    Nov-21-2023_4_00_06_PM

--- fizz.py
+++ fizz.py
@@ -4,6 +4,6 @@ for i in range(1, 101):
     output += "Fizz"
   if i % 5 == 0:
     output += "Buzz"
-  if i % 7 == 0:
-    output += "Bazz"
+#  if i % 7 == 0:
+#    output += "Bazz"
   print(output or i)
