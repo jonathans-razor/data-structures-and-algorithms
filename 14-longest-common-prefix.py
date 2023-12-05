107b6df1380d26c62e2fcf8f16fe04a19d9c9416
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-12-05 14:08:16 -0500

    Dec-5-2023_2_08_16_PM

--- 14-longest-common-prefix.py
+++ 14-longest-common-prefix.py
@@ -1,22 +1,29 @@
 import sys
-from collections import defaultdict
 
+"""
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+return type: string
+"""
 def findLongestCommonPrefix(strs):
-  answer = ""
-  sortedListOfStrings = sorted(strs)
-  firstWord = sortedListOfStrings[0]
-  lastWord = sortedListOfStrings[-1]
-  print("fw: " + firstWord, "lw: " + lastWord)
-  print("1w: " + str(len(firstWord)))
-  length = min(len(firstWord), len(lastWord))
-  print("length: " + str(length))
-  for i in range(min(len(firstWord), len(lastWord))):
-    if(firstWord[i] != lastWord[i]):
-      return answer
-    answer += firstWord[i]
-  return answer
+    answer = ""
+    if len(strs) == 0:  
+        return answer
+    if len(strs) == 1:
+        return strs[0]
+    strs.sort()
+    first = strs[0]
+    last = strs[-1]
+    for i in range(len(first)):
+        if first[i] == last[i]:
+            answer += first[i]
+        else:
+            break
+    return answer
 
-if __name__ == '__main__':
-  strs = sys.argv[1:]
-  longestCommonPrefix = findLongestCommonPrefix(strs)
-  print(longestCommonPrefix)
+if __name__ == "__main__":
+    strs = sys.argv[1:]
+    longestCommonPrefix = findLongestCommonPrefix(strs)
+    print(longestCommonPrefix)
