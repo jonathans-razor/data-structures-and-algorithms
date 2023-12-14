d73e4a3127d065788e0b22f85ed305a9ba6b46eb
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-12-14 13:46:47 -0500

    Dec-14-2023_1_46_47_PM

--- 43-multiply-strings.py
+++ 43-multiply-strings.py
@@ -4,23 +4,30 @@ if len(sys.argv) < 2:
   print("- Parameter(s) expected.")
   sys.exit(1)
 
-class Solution:
-    def multiply(self, num1: str, num2: str) -> str:
-        '''
-        Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+def multiply(num1: str, num2: str) -> str:
+    '''
+    Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
 
-        Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.      
-        Return Value:
-        '''
-        # 1. Convert the strings to integers
+    Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.      
+    Return Value:
+    '''
+    if num1 == "0" or num2 == "0":
+        return "0"
+    m, n = len(num1), len(num2)
+    res = [0] * (m + n)
+    for i in range(m - 1, -1, -1):
+        for j in range(n - 1, -1, -1):
+            multiplied_result = int(num1[i]) * int(num2[j])
+            p1, p2 = i + j, i + j + 1
+            sum = multiplied_result + res[p2]
+            res[p2] = sum % 10
+            res[p1] += sum // 10
+    i = 0
+    while i < len(res) and res[i] == 0:
+        i += 1
+    return "".join(map(str, res[i:]))
 
-        # 2. Multiply the integers
-        # 3. Convert the result back to string
-        # 4. Return the result
-        return
-
-if __name__ == '__main__':
-    nums = list(map(int, sys.argv[1].split(','))) if sys.argv[1] else []
-    #target = int(sys.argv[2])
-    solution = Solution()
-    print(solution.x(nums))
+if __name__ == "__main__":
+    num1 = sys.argv[1]
+    num2 = sys.argv[2]
+    print(multiply(num1, num2))
