1197d6b721c1169399606252e750831c5e7a2c8c
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-12-15 13:14:28 -0500

    Dec-15-2023_1_14_28_PM

--- 44-wildcard-matching.py
+++ 44-wildcard-matching.py
@@ -5,12 +5,30 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 class Solution:
-    def isMatch(self, s: str, p: str) -> bool:
+    def isMatch(self, s: str, pattern: str) -> bool:
         """
-
-        Return Value:
+        Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*' where:
+        '?' Matches any single character.
+        '*' Matches any sequence of characters (including the empty sequence).
+        The matching should cover the entire input string (not partial).
+        Return Value: bool
         """
-        return False
+        s_len = len(s)
+        pattern_len = len(pattern)
+        dp = [[False] * (pattern_len + 1) for _ in range(s_len + 1)]
+        dp[0][0] = True
+        for i in range(1, pattern_len + 1):
+            if pattern[i - 1] == '*':
+                dp[0][i] = True
+            else:
+                break
+        for i in range(1, s_len + 1):
+            for j in range(1, pattern_len + 1):
+                if pattern[j - 1] == '*':
+                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1] 
+                elif pattern[j - 1] == '?' or s[i - 1] == pattern[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+        return dp[s_len][pattern_len]
 
 if __name__ == "__main__":
     solution = Solution()
