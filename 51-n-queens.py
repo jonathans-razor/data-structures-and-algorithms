dfff30aa7901fa1a2d1bcd63f1b2050921536796
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-12-20 13:16:17 -0500

    Dec-20-2023_1_16_17_PM

--- 51-n-queens.py
+++ 51-n-queens.py
@@ -15,8 +15,17 @@ class Solution:
         Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
         return value: List[List[str]]
         """
-        return [["Q"]]
-
+        def dp(queens, xy_diff, xy_sum):
+            p = len(queens)
+            if p == n:
+                result.append(queens)
+                return
+            for q in range(n):
+                if q not in queens and p - q not in xy_diff and p + q not in xy_sum:
+                    dp(queens + [q], xy_diff + [p - q], xy_sum + [p + q])
+        result = []
+        dp([], [], [])
+        return [["." * i + "Q" + "." * (n - i - 1) for i in sol] for sol in result]
 
 if __name__ == "__main__":
     solution = Solution()
