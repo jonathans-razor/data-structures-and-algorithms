334a4763fc7d0e1f235b9350bbfa7f5a3ecf479f
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2023-12-28 14:20:43 -0500

    Dec-28-2023_2_20_42_PM

--- 58-length-of-last-word.py
+++ 58-length-of-last-word.py
@@ -1,12 +1,18 @@
 import sys
 
-def lengthOfLastWord(s: str) -> int:
-  if(len(s) == 0):
-    return 0
-  words = s.split()
-  if(len(words) == 0):
-    return 0
-  return len(words[-1])
 
-if __name__ == '__main__':
-  print(lengthOfLastWord(sys.argv[1]))
+def lengthOfLastWord(s: str) -> int:
+    """
+    Given a string s consisting of words and spaces, return the length of the last word in the string.
+    A word is a maximal substring consisting of non-space characters only.
+    return value: int
+    """
+    if len(s) == 0:
+        return 0
+    words = s.split()
+    if len(words) == 0:
+        return 0
+    return len(words[-1])
+
+if __name__ == "__main__":
+    print(lengthOfLastWord(sys.argv[1]))
