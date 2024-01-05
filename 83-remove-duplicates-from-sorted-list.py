10d31db5c18095da7e78ef9bd4541a97703ea9e2
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-01-05 13:30:18 -0500

    Jan-5-2024_1_30_17_PM

--- 83-remove-duplicates-from-sorted-list.py
+++ 83-remove-duplicates-from-sorted-list.py
@@ -1,20 +1,39 @@
 import sys
 from typing import Optional
-from typing import Optional, List
 
 if len(sys.argv) < 2:
-  print("- Parameter(s) expected.")
-  sys.exit(1)
+    print("- Parameter(s) expected.")
+    sys.exit(1)
 
-def deleteDuplicates(head: Optional[ListNode]) -> Optional[ListNode]:
-  if head is None:
-    return None
-  current = head
-  while current.next is not None:
-    if current.val == current.next.val:
-      current.next = current.next.next
-    else:
-      current = current.next
-  return head
+# Definition for singly-linked list.
+class ListNode:
+  def __init__(self, val=0, next=None):
+    self.val = val
+    self.next = next
 
-print(deleteDuplicates(sys.argv[1]))
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+      '''
+      Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+      Example 1:
+
+      Input: head = [1,1,2]
+      Output: [1,2]
+      Example 2:
+
+      Input: head = [1,1,2,3,3]
+      Output: [1,2,3]
+
+      Constraints:
+
+      The number of nodes in the list is in the range [0, 300].
+      -100 <= Node.val <= 100
+      The list is guaranteed to be sorted in ascending order.
+      return ListNode
+      '''
+      return head 
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.deleteDuplicates(sys.argv[1]))
