d1396afd1469cd45668a4034437a8e47cf347f58
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-01-14 23:07:09 -0500

    Jan-14-2024-11-07-09-PM

--- email-regex-validation.js
+++ email-regex-validation.js
@@ -1,3 +1,10 @@
+/*
+Create a JavaScript program that does email regex validation. The program should accept and 
+email address as input and output a boolean as to whether the input was a valid email address. 
+Automatically pass in the data from the command line.
+
+::Jan-14-2024
+*/
 const email = process.argv[2];
 const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const isValidEmail = regex.test(email);
