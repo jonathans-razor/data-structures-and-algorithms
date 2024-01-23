3c974ba4d5aa920cf246bbdc2b36de9fa0b87f7d
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-01-23 16:31:49 -0500

    Jan-23-2024-4-31-49-PM

---
+++ optional-chaining.js
@@ -0,0 +1,16 @@
+/*
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+*/
+const adventurer = {
+  name: 'Alice',
+  cat: {
+    name: 'Dinah',
+  },
+};
+
+const dogName = adventurer.dog?.name;
+console.log(dogName);
+// Expected output: undefined
+
+console.log(adventurer.someNonExistentMethod?.());
+// Expected output: undefined
