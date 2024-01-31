2a63be8bad26572a8e787765167eb3aded649fa0
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-01-30 20:44:34 -0500

    Jan-30-2024-8-44-34-PM

--- vue-button-2/my-project/src/App.vue
+++ vue-button-2/my-project/src/App.vue
@@ -1,26 +1,14 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<template>
+  <button @click="count++">Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
 }
 </style>
